5500a15695d4b2d9b56d58a893ecf028d0abcbe8
isyoo1213 <moethns@gmail.com>
2023-03-22 00:26:56 +0900

    feat: Add Config for using ThreadLocalLogTracer

--- advanced/src/main/java/spring/advanced/LogTraceConfig.java
+++ advanced/src/main/java/spring/advanced/LogTraceConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.advanced.trace.logTrace.FieldLogTrace;
 import spring.advanced.trace.logTrace.LogTrace;
+import spring.advanced.trace.logTrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        //return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
