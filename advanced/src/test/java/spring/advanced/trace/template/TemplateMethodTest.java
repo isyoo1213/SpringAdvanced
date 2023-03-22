022711fd41c2631a4b1b4445ebed28e50bf63788
isyoo1213 <moethns@gmail.com>
2023-03-22 12:43:54 +0900

    feat: Add patternV2 Test using InnerAnonymousClass

--- advanced/src/test/java/spring/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/spring/advanced/trace/template/TemplateMethodTest.java
@@ -23,6 +23,32 @@ class TemplateMethodTest {
         template1.execute();
     }
 
+    /**
+     * 익명내부클래스를 통해 TemplateMethod의 AbstractTemplate 클래스를 상속받는 구현 클래스 정의를 생략
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+
+        // * 익명내부클래스는 이름이 없으므로 이를 생성하는 위치에서 &를 통해 구분
+        log.info("익명내부클래스1 이름 = {}", template1.getClass());
+        log.info("익명내부클래스2 이름 = {}", template2.getClass());
+        template1.execute();
+        template2.execute();
+    }
+
     private void logic1() {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행 가정
