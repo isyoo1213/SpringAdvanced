2fb839b7bc6dbcbb167e584484017e9b33747eda
isyoo1213 <moethns@gmail.com>
2023-03-28 18:02:05 +0900

    feat: Add AppV2 - Non Interface in ManualBeanRegistation

---
+++ proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping
+//수동 등록을 위해 @Controller 사용 X
+//@RequestMapping은 Component Scan의 대상이 아님
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
