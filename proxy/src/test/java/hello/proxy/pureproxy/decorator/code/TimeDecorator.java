7050ebb12f11952c91bc38270e1dc71d1a870e87
isyoo1213 <moethns@gmail.com>
2023-04-02 14:49:51 +0900

    feat: Add TimeDecorator for Decorator-Chan

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -3,25 +3,27 @@ package hello.proxy.pureproxy.decorator.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class DecoratorPatternClient {
+public class TimeDecorator implements Component{
 
     private Component component;
 
-    public DecoratorPatternClient(Component component) {
+    public TimeDecorator(Component component) {
         this.component = component;
     }
 
-    public void execute() {
-
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
         long startTime = System.currentTimeMillis();
 
         String result = component.operation();
 
         long endTime = System.currentTimeMillis();
-
         long resultTime = endTime - startTime;
 
-        log.info("result = {}", result);
+        log.info("TimeDecorator 종료");
         log.info("resultTime = {}", resultTime);
+
+        return result;
     }
 }
